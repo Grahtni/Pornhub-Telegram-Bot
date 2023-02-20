d20e51485565e8c21b1a29b653da8ec11714e84e
Zubin <me@zubin.org.in>
2023-02-20 13:17:47 +0530

    Add Vercel config

--- api/bot.js
+++ api/bot.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 const { Bot, webhookCallback } = require("grammy");
 const pornhub = require("@justalk/pornhub-api");
+const regex = /https?:\/\/(www\.)?pornhub\.(com|org)\/[a-zA-Z0-9_\.]+\/?/;
 
 // Bot
 
 const bot = new Bot(process.env.BOT_TOKEN);
+
+// DB
+
+const mysql = require("mysql2");
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 
 // Commands
 
@@ -13,7 +19,37 @@ bot.command("start", async (ctx) => {
     .reply("*Welcome!* ✨ Send a Pornhub link.", {
       parse_mode: "Markdown",
     })
-    .then(console.log("New user added:\n", ctx.from))
+    .then(
+      connection.query(
+        `
+SELECT * FROM users WHERE userid = ?
+`,
+        [ctx.from.id],
+        (error, results) => {
+          if (error) throw error;
+          if (results.length === 0) {
+            connection.query(
+              `
+    INSERT INTO users (userid, username, firstName, lastName, firstSeen)
+    VALUES (?, ?, ?, ?, NOW())
+  `,
+              [
+                ctx.from.id,
+                ctx.from.username,
+                ctx.from.first_name,
+                ctx.from.last_name,
+              ],
+              (error, results) => {
+                if (error) throw error;
+                console.log("New user added:", ctx.from);
+              }
+            );
+          } else {
+            console.log("User exists in database.", ctx.from);
+          }
+        }
+      )
+    )
     .catch((e) => console.error(e));
 });
 
@@ -30,21 +66,32 @@ bot.command("help", async (ctx) => {
 // Messages
 
 bot.on("msg", async (ctx) => {
-  try {
-    const url = ctx.msg.text;
-    console.log("Query:", url, "by", ctx.from.id);
-    const video = await pornhub.page(url, ["title", "download_urls"]);
-    console.log(video);
-    console.log(video.title);
-    await ctx.reply(video.title, { reply_to_message_id: ctx.msg.message_id });
-    await ctx.reply(video.download_urls["480P"]);
-    await ctx.replyWithDocument(video.download_urls["480P"]);
-  } catch (error) {
-    console.error(error);
-    await ctx.reply(`*An error occured.*`, {
+  if (!regex.test(ctx.msg.text)) {
+    await ctx.reply("*Send a Pornhub video link.*", {
       parse_mode: "Markdown",
       reply_to_message_id: ctx.msg.message_id,
     });
+  } else {
+    const status = await ctx.reply("*Downloading*", { parse_mode: "Markdown" });
+    setTimeout(async () => {
+      await ctx.api.deleteMessage(ctx.chat.id, status.message_id);
+    }, 5000);
+    try {
+      const url = ctx.msg.text;
+      console.log("Query:", url, "by", ctx.from.id);
+      const video = await pornhub.page(url, ["title", "download_urls"]);
+      await ctx.reply(video.title, { reply_to_message_id: ctx.msg.message_id });
+      await ctx.reply(video.download_urls["480P"]);
+    } catch (error) {
+      console.error(error);
+      await ctx.reply(
+        `*An error occured.*\n_Are you sure the link is correct?_`,
+        {
+          parse_mode: "Markdown",
+          reply_to_message_id: ctx.msg.message_id,
+        }
+      );
+    }
   }
 });
 
